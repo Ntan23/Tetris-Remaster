f23397add9c7f46d985882e8b399c614c38f7c4c
fajarnadril <72092638+CloterChohan@users.noreply.github.com>
2023-10-12 16:02:27 +0700

    Added DeathRow
    Still not implemented yet

--- Assets/Scripts/Not Use Tiles/Player/PlayerMovement.cs
+++ Assets/Scripts/Not Use Tiles/Player/PlayerMovement.cs
@@ -293,6 +293,14 @@ public class PlayerMovement : MonoBehaviour
     {
         audioManager.PlayTeleport();
     }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.CompareTag("DeathArea"))
+        {
+            gm.GameOver();
+        }
+    }
     
     IEnumerator Wait()
     {
